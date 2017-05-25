7d0270e02b7ee6fa84a5b7d4feea89c644b5e1a3
Ondrej Kratochvil <ondrej.kratochvil@gmail.com>
2017-05-25 22:56:14 +0200

    Remove 2 unecessary chars by making line counter start at 1 instead of 0

--- avast_menu.5m.py
+++ avast_menu.5m.py
@@ -19,14 +19,14 @@ for table in menu_lxml.xpath("//div[@class='row'][1]//table"):
 	print("---")
 	print(days[i].upper()," | color=red")
 	i = i + 1
-	n = 0
+	n = 1
 	for item in table.xpath(".//td[contains(@class,'meal_name')]/text()[1]"):
 	 	prefix = ""
 	 	color = "gray"
-	 	if n < 2:
+	 	if n <= 2:
 	 		prefix = "[P] "
 	 		color = "green"
-	 	if n == len(table) - 1:
+	 	if n == len(table):
 	 		prefix = "[S] "
 	 		color = "blue"
 	 	colorcode = " | color=" + color
